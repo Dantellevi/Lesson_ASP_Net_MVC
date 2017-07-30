fe91a562fffc9b9db52cd1052bb37c59396033df
Alex <dante.l.levi93@gmail.com>
2017-07-30 22:49:09 +0400

    one to many add CRUD-function

--- Metanit_work_with_hard_database/Metanit_work_with_hard_database/Controllers/HomeController.cs
+++ Metanit_work_with_hard_database/Metanit_work_with_hard_database/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace Metanit_work_with_hard_database.Controllers
         }
 
         // GET: Список игроков
+        //==============================================================================
         public ActionResult Index()
         {
 
@@ -27,9 +28,9 @@ namespace Metanit_work_with_hard_database.Controllers
             return View(pl.ToList());
 
         }
+        //=========================================================================
 
-
-
+        //=========================================================================
         //описание игроков
         public ActionResult Details(int? Id)
         {
@@ -52,15 +53,92 @@ namespace Metanit_work_with_hard_database.Controllers
 
 
         }
+        //=======================================================================
+        //Добавляем объект
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Первый метод обрабатывает GET-запрос и возвращает представление, 
+        //передавая в него объект SelectList - список всех команд.
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //формируем список команд для передачи в представление
+            SelectList teams = new SelectList(db.teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+
+
+        }
+
+        //Второй метод получает введенную пользователем
+        //в представлении модель и добавляет ее в БД.
+
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            //добавляем игрока в таблицу
+            db.players.Add(player);
+            db.SaveChanges();
+
+            //перенаправляем на главную страницу
+            return RedirectToAction("Index");
+
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+        //Редактирование модели
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Здесь также в виде объекта SelectList создается список команд,
+        //которые извлекаются из БД. И после получения запроса на редактирование 
+        //определенной модели Player контроллер передает эту модель и список команд
+        //в представление
+        [HttpGet]
+        public ActionResult Edit(int? Id)
+        {
+            if(Id==null)
+            {
+                return HttpNotFound();
+            }
+
+            //Находим в бд футболиста
+            Player player = db.players.Find(Id);
+            if(player!=null)
+            {
+                //Создаем список команд для передачи в представление
+                SelectList teams = new SelectList(db.teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+           
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            
+        }
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         //список команд
+        //======================================================================
         public ActionResult ListTeams()
         {
             var teamspls = db.teams.Include(t => t.Players);
             return View(teamspls.ToList());
         }
+        //======================================================================
 
         //Описание команды
+        //=======================================================================
         public ActionResult TeamDetails(int? Id)
         {
             if(Id==null)
@@ -72,6 +150,7 @@ namespace Metanit_work_with_hard_database.Controllers
             return View(team);
 
         }
+        //========================================================================
             
 
 
